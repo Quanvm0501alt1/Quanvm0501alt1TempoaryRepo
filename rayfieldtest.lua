34e095528740462b1c85a4805c5fe1cb9ac54ded
Quanvm0501alt1 <quanvm0501alt1@tutamail.com>
2024-10-25 13:37:54 +0700

    Update rayfieldtest.lua

--- rayfieldtest.lua
+++ rayfieldtest.lua
@@ -68,5 +68,12 @@ local Button = Main:CreateButton({
         Notify("Notification", "Button pressed!")
     end,
 })
-
+local Toggle = Main:CreateToggle({
+   Name = "Toggle Example",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   Notify("Toggle1", value)
+   end,
+})
 Rayfield:LoadConfiguration()
