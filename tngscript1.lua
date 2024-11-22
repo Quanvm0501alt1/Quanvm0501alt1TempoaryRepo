f09d7a8f26023ba67624aef0377860cdaf7597f4
Quân Vũ Minh 💀 (Python, C++, Lua) <quanvm0501alt1@tutamail.com>
2024-11-22 23:02:10 +0700

    Create tngscript1.lua

---
+++ tngscript1.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "Test",--Tên Script
+    logoscript = "114512310830805",--ID Logo Script
+    tacgia = "vmquanalt1"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
